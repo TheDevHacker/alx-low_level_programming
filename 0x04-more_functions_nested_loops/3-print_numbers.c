e4c788041323ccae6fad2e8ba2215b27b0299dfc
Nathan <nathanavaaha@gmail.com>
2023-12-22 00:48:46 +0300

    all

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_numbers - print all numbers from 0 to 9
+*Return: it returns nothing
+*/
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+_putchar(number);
+}
+_putchar(10);
+}
