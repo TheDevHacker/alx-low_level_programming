a3a770aa01e461414c88ba0b60f10493ca84c2ab
Nathan Avaaga <nathanavaaga@gmail.com>
2024-02-01 23:14:27 +0300

    prototype error fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,12 +2,12 @@
 #include "lists.h"
 
 /**
- * lst_len - returns  elements in a linked list
+ * list_len - returns  elements in a linked list
  * @h: pointer
  *
  * Return: number of elements in h
  */
-size_t lst_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t n = 0;
 
