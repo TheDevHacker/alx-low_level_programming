36cdc5c987b6fcb9e0776baacf668de6bc01416e
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-26 06:07:16 +0300

    shared lib

---
+++ 0x09-static_libraries/add.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * add - adds numbers
+ * @a: - first
+ * @b: - second
+ * Return: addition
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
---
+++ 0x09-static_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * sub - subs numbers
+ * @a: - first
+ * @b: - second
+ * Return: subtraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
